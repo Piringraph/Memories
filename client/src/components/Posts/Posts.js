09f69f254aa492a4ccd5591980121fe3759794da
Pirin <chuckdanorris123@gmail.com>
2022-10-30 23:29:02 -0400

    added Redux functionalities

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,24 @@
+import React from "react";
+import useStyles from './styles'
+import { useSelector } from "react-redux";
+
+import Post from './Post/Post.js';
+
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+
+    console.log(posts);
+
+
+    return (
+        <>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
+        </>
+    );
+}
+
+export default Posts;
