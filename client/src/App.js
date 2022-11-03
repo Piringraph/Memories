ad2bf27c90d5fd12e6da735e3c90f5e800ca2f84
Pirin <chuckdanorris123@gmail.com>
2022-11-02 23:40:47 -0400

    added POST functionality

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
     useEffect(() => {
         dispatch(getPosts());
-    }, [dispatch])
+    }, [dispatch]);
 
     return (
         <Container maxwidth="lg">
--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,6 +10,14 @@ export const getPosts = () => async (dispatch) => {
         console.log(error.message);
         }
 
-    const action = {  }
+}
 
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+        console.log(error);
+    }
 }
